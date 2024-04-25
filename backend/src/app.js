8499cf79586ba5cac4aef8a320a2d92216653bd0
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2024-04-25 20:28:02 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: ["https://wedding-sparks-6nxj-frontend.vercel.app/"],
     credentials: true,
   })
 );
