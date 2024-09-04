2d1d94bae6b930bc2bd86af3a4a386885e784ff8
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2024-09-04 16:46:51 +0530

    Update vite.config.js
    add backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/users": "https://wedding-spark-backend.vercel.app/",
+    },
+  },
   plugins: [react()],
 })
