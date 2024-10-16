6073f41920437953939d2e5f2e6cb94d488e5450
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2024-10-16 22:04:34 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,21 +20,6 @@ export default defineConfig({
           });
         },
       },
-      "/venders": {
-        target: "https://wedding-spark-backend.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false,
-        configure: (proxy, options) => {
-          proxy.on("proxyRes", (proxyRes, req, res) => {
-            proxyRes.headers["Access-Control-Allow-Origin"] = "https://wedding-spark-frontend.vercel.app";
-            proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
-            proxyRes.headers["Access-Control-Allow-Methods"] = "GET,OPTIONS,PATCH,DELETE,POST,PUT";
-            proxyRes.headers["Access-Control-Allow-Headers"] =
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version";
-          });
-        },
-      },
     },
   },
   plugins: [react()],
