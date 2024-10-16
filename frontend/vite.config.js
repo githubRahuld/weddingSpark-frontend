790f2882df1f43b85de2317295e8f73d0c8e9e58
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2024-10-16 21:34:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,11 +11,9 @@ export default defineConfig({
         secure: false,
         configure: (proxy, options) => {
           proxy.on("proxyRes", (proxyRes, req, res) => {
-            proxyRes.headers["Access-Control-Allow-Origin"] =
-              "https://crazzzy-tube-frontend.vercel.app";
+            proxyRes.headers["Access-Control-Allow-Origin"] = "https://wedding-spark-frontend.vercel.app";
             proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
-            proxyRes.headers["Access-Control-Allow-Methods"] =
-              "GET,OPTIONS,PATCH,DELETE,POST,PUT";
+            proxyRes.headers["Access-Control-Allow-Methods"] = "GET,OPTIONS,PATCH,DELETE,POST,PUT";
             proxyRes.headers["Access-Control-Allow-Headers"] =
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version";
           });
@@ -27,11 +25,9 @@ export default defineConfig({
         secure: false,
         configure: (proxy, options) => {
           proxy.on("proxyRes", (proxyRes, req, res) => {
-            proxyRes.headers["Access-Control-Allow-Origin"] =
-              "https://crazzzy-tube-frontend.vercel.app";
+            proxyRes.headers["Access-Control-Allow-Origin"] = "https://wedding-spark-frontend.vercel.app";
             proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
-            proxyRes.headers["Access-Control-Allow-Methods"] =
-              "GET,OPTIONS,PATCH,DELETE,POST,PUT";
+            proxyRes.headers["Access-Control-Allow-Methods"] = "GET,OPTIONS,PATCH,DELETE,POST,PUT";
             proxyRes.headers["Access-Control-Allow-Headers"] =
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version";
           });
