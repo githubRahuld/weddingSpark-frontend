badeab3520bf2eb7bec5afc856e9bf65f923713b
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2024-10-16 17:10:58 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/users": "https://wedding-spark-backend.vercel.app",
-      "/venders": "https://wedding-spark-backend.vercel.app",
+      "/users": {
+        target: "https://wedding-spark-backend.vercel.app",
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, options) => {
+          proxy.on("proxyRes", (proxyRes, req, res) => {
+            proxyRes.headers["Access-Control-Allow-Origin"] =
+              "https://crazzzy-tube-frontend.vercel.app";
+            proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
+            proxyRes.headers["Access-Control-Allow-Methods"] =
+              "GET,OPTIONS,PATCH,DELETE,POST,PUT";
+            proxyRes.headers["Access-Control-Allow-Headers"] =
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version";
+          });
+        },
+      },
+      "/venders": {
+        target: "https://wedding-spark-backend.vercel.app",
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, options) => {
+          proxy.on("proxyRes", (proxyRes, req, res) => {
+            proxyRes.headers["Access-Control-Allow-Origin"] =
+              "https://crazzzy-tube-frontend.vercel.app";
+            proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
+            proxyRes.headers["Access-Control-Allow-Methods"] =
+              "GET,OPTIONS,PATCH,DELETE,POST,PUT";
+            proxyRes.headers["Access-Control-Allow-Headers"] =
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version";
+          });
+        },
+      },
     },
   },
   plugins: [react()],
-})
+});
